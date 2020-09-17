43d7446b82ffe1bfe6e3e655a581499cf11cb10b
gmarszalek <grzegorzmarszalek92@gmail.com>
2020-09-17 20:43:18 +0200

    Updates

---
+++ precache-manifest.158bccda1845ff4d3638ce7d8dd2f3fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27dc73395a23e58ce51c21eced60e0da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "593b5b7ff3ec03bf93bd",
+    "url": "/static/css/main.a99200be.chunk.css"
+  },
+  {
+    "revision": "9a1df8cf20f2a9b060b9",
+    "url": "/static/js/2.ca14278d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca14278d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "593b5b7ff3ec03bf93bd",
+    "url": "/static/js/main.21b3f270.chunk.js"
+  },
+  {
+    "revision": "eb323c622e106faecb0e",
+    "url": "/static/js/runtime-main.341c860d.js"
+  }
+]);
